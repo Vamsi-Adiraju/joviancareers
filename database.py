b2cdfd71ade93622916e339a0f296f35f6ae7f00
Vamsi Adiraju <91866331+Vamsi-Adiraju@users.noreply.github.com>
2022-11-10 05:30:13 +0000

    added jobpage

--- database.py
+++ database.py
@@ -24,3 +24,14 @@ def get_jobs_rn():
     for job in lst:
       jobs.append(dict(job))
   return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"),
+                         val =id)
+    job = result.all()
+    if len(job) == 0:
+      return None
+    else:
+      return dict(job[0])
+    
